bbaadfeff810e293a9931c05d48d44199728df5b
dodo7772772 <126598579+tyywqtwui88ss@users.noreply.github.com>
2023-04-15 18:18:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
